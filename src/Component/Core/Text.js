71ecbb65c65d4ff198e2a456076d184b0de308d8
Unknows <firstpan555@gmail.com>
2019-09-05 16:05:37 +0700

    [ADD]Text

---
+++ src/Component/Core/Text.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components'
+import fonts from '../../Config/Fonts';
+import color from '../../Config/Color';
+
+export const Header = styled.div`
+    font-size: ${fonts.Headline};
+    color:${color.name};
+`
+
+export const HeaderBold = styled.div`
+    font-size: ${fonts.Headline};
+    font-weight:bold;
+    color:${color.name};
+`
+
+export const Paragraph = styled.div`
+    font-size: ${fonts.Paragraph};
+    color:${color.name};
+`
+
+export const ParagraphBold = styled.div`
+    font-size: ${fonts.Paragraph};
+    font-weight:bold;
+    color:${color.name};
+`
+
+export const Subtitle = styled.div`
+    font-size: ${fonts.Subtitle};
+    color:${color.name};
+`
+
+export const SubtitleBold = styled.div`
+    font-size: ${fonts.Subtitle};
+    font-weight:bold;
+    color:${color.name};
+`
+
+export const Small = styled.div`
+    font-size: ${fonts.Small};
+    color:${color.name};
+`
+
+export const SmallBold = styled.div`
+    font-size: ${fonts.Small};
+    font-weight:bold;
+    color:${color.name};
+`
+
