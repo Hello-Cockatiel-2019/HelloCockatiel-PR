622bb7ce3ed70e0da234e3e9fafa776380a53b7a
Unknows <firstpan555@gmail.com>
2019-09-05 23:10:55 +0700

    [ADD]Fontsize and Fontfamily

---
+++ src/Config/Fonts.js
@@ -0,0 +1,24 @@
+const QuarkFonts = {
+    Subtitle: '30px',
+    Paragraph: '20px',
+    Small: '18px',
+    MParagraph: '18px',
+    MSmall: '14px'
+}
+export const PaaymaayFonts = {
+    Headline : '40px',
+    Subtitle: '36px',
+    Paragraph: '34px',
+    Small: '20px',
+    MHeadline:'30px',
+    MSubtitle: '28px',
+    MParagraph: '24px',
+    MSmall:'14px'
+}
+
+export const FontFamily = {
+    QuarkFonts : 'Quark',
+    PaaymaayFonts : 'Paaymaay'
+}
+
+export default QuarkFonts
