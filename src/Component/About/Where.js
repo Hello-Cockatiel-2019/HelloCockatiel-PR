47686e0556105d9235a6a8e679eca533417ca7b8
Poony <poon.002@hotmail.com>
2019-09-08 05:51:39 +0700

    [UPDATE]Textbox with responsive

--- src/Component/About/Where.js
+++ src/Component/About/Where.js
@@ -3,26 +3,58 @@ import styled from 'styled-components'
 
 import { Paragraph } from '../Core/Text'
 
-const Sit = styled.div `
-    background-image: url(/images/Objects/arrowplate2.png);
-    background-repeat: no-repeat;
-    height: 100%;
-    width: 100%;
-    background-size:100%;
-    padding-left:0.5em;
-    padding-top:0.2em;
-`
 const Where1 = styled.div `
+@media(min-width:320px) {
+    
+    width: 85px;
+    height: 25px;
+}
+@media (min-width:410px) {
+    height: 3.4375em;
+    width: 11.3125em;
+}
+@media (min-width:576px) {
+    height: 3.4375em;
+    width: 11.3125em;
+}
     background-image: url(/images/Objects/arrowplate3.png);
     background-repeat: no-repeat;
-    height: 4.9375em;
-    width: 17.1875em;
-    size:100%;
-    padding-left:0.5em;
-    padding-top:0.2em;
+    background-size:100%;
+    padding-left:2em;
+    margin-left:0.1em;
+    
 `
+const Sit = styled.div `
+    background-image: url(/images/Objects/arrowplate2.png);
+    @media(min-width:320px) {
+        height: 38px;
+        width: 131px;
+    }
+    @media (min-width:410px) {
+        height: 4.9375em;
+        width: 17.1875em;
+    }
+    @media (min-width:576px) {
+        height: 4.9375em;
+        width: 17.1875em;
+    }
+    background-repeat: no-repeat;
+    background-size:100%;
+    padding-left:0.5em;
+    margin-top:1em;
+`
+
 const Text= styled(Paragraph) `
     transform: translateY(50%);
+    @media(min-width:320px) {
+        
+    }
+    @media (min-width:410px) {
+        
+    }
+    @media (min-width:576px) {
+        
+    }
 `
 
 export default class Where extends Component{
@@ -32,7 +64,7 @@ export default class Where extends Component{
             <div class="d-flex flex-column justify-content-around">
                 <Where1>
                     <Text>
-                    สถานที่
+                    สถานที่จัด
                     </Text>
                 </Where1>
                 <Sit>
--- src/Component/About/index.js
+++ src/Component/About/index.js
@@ -5,14 +5,57 @@ import Where from './Where'
 import { Paragraph } from '../Core/Text'
 
 const Plate = styled.div `
-    background-image: url(/images/Objects/big-plate.png);
+    
+    @media(min-width:320px) {
+        background-image: url(/images/Objects/small-plate.png);
+        width:263px;
+        height: 218px;
+      }
+      @media (min-width:410px) {
+        background-image: url(/images/Objects/small-plate.png);
+        width:340px;
+        height: 283px;
+      }
+    //   @media (min-width:576px) {
+    //     background-image: url(/images/Objects/small-plate.png);
+    //     width:470px;
+    //     height: 443px;
+    //   }
+    //   @media (min-width:768px) {
+    //     background-image: url(/images/Objects/big-plate.png);
+    //     width:740px;
+    //     height: auto;
+    //   }
+    //   @media(min-width:1024px) {
+    //     background-image: url(/images/Objects/big-plate.png);
+    //     height: 25.1875em;
+    //     width: auto;
+    //   }
+    //   @media only screen and (min-width: 768px) and (max-height: 1024px) and (orientation: portrait)  {
+    //     background-image: url(/images/Objects/big-plate.png);
+    //     height: 25.1875em;
+    //     width: auto;
+    //   }
+    //   @media only screen and (min-width: 1024px) and (max-height: 1370px) and (orientation: portrait)  {
+    //     background-image: url(/images/Objects/big-plate.png);
+    //     height: 25.1875em;
+    //     width: 43.75em;
+    //   } 
+      background-size:100%; 
     background-repeat: no-repeat;
-    size:100%;
-    height: 25.1875em;
-    width: 43.75em;
+    // size:100%;
+    
 `
 const Text= styled(Paragraph) `
+    @media(min-width:320px) {
     
+    }
+    @media (min-width:410px) {
+    
+    }
+    @media (min-width:576px) {
+    
+    }
 `
 
 const AboutBox = () => (
