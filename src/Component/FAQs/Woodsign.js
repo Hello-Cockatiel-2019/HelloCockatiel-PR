ba621016c9d17a009c1647801f6d80ba291c1165
Dan Kent <patorn.5@mail.kmutt.ac.th>
2019-09-07 23:22:23 +0700

    [UPDATE]wooden sign

--- src/Component/FAQs/Woodsign.js
+++ src/Component/FAQs/Woodsign.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import { Paragraph, BrownParagraph } from '../Core/Text'
 
-const Qwood = styled.div `
+const Qwood = styled(BrownParagraph) `
     background-color :#DB9B51;
     width: 462px;
     height: 38px;
     padding-left:0.7em;
     padding-top:0.3em;
+    margin-top: 0.5em;
 `
-const Awood = styled.div `
+const Awood = styled(Paragraph)`
     background-color:#987245;
     width: 462px;
     padding-top:0.5em;
     padding-bottom: 1em;
     padding-left:0.7em;
+    margin-bottom: 3em;
 `
 
 export default class Woodsign extends Component {
     render() {
         return (
-                <div className="d-flex flex-column">
+                <div className="d-flex flex-column pr-2">
                 <Qwood>Q: {this.props.q}</Qwood>
                 <Awood>A: {this.props.a}</Awood>
                 </div>
