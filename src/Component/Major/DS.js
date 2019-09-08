49d70badd26d204a879b2ac1d8c38bb67a6c524a
poomktw <kittiwat.ktw@mail.kmutt.ac.th>
2019-09-08 18:18:43 +0700

    [UPDATE]edit scale and responsive i5

--- src/Component/Major/DS.js
+++ src/Component/Major/DS.js
@@ -1,31 +1,92 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import { Small, Small2 } from '../Core/Text';
 
 const DSbg = styled.div`
     background-color :#DB9B51;
     opacity: 0.9;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     border-radius: 30px;
-    width: 25em;
-    height: 9em;
+    width: 27em;
+    height: 10em;
+    @media screen and (max-width: 600px){
+        width:19em;
+        height:7em;
+    }
 `
 const Bird = styled.img`
-box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), 
-            0px 4px 4px rgba(0, 0, 0, 0.25), 
-            0px 4px 4px rgba(0, 0, 0, 0.25), 
-            0px 4px 4px rgba(0, 0, 0, 0.25), 
-            0px 4px 4px rgba(0, 0, 0, 0.25), 
-            0px 4px 4px rgba(0, 0, 0, 0.25);
-            width:20%;
+    padding-left:2em;
+    padding-top:0.7em;
+    width:7.5em;
+    @media screen and (max-width: 600px){
+        width:6em;
+        margin-top:-0.3em;
+        transform: matrix(-1, 0, 0, 1, 0, 0);
+        margin-left:13.5em;
+    }
 `
 const Frame = styled.img`
-width:35%;
+    margin-left:2em;
+    width:6em;
+    position:absolute;
+    z-index: 2;
+    padding-top:6.2em;
+    margin-left:-5.7em;
+    @media screen and (max-width: 600px){
+        width:4.5em;
+        margin-left:13.5em;
+        padding-top:0em;
+    }
 `
+const Text = styled(Small)`
+    font-family: Quark;
+    font-size: 18px;
+    line-height: 25px;
+    letter-spacing: 0.05em;
+    position:absolute;
+    z-index: 3;
+    padding-left:8em;
+    margin-top:-4.8em;
+    color: #000000;
+    @media screen and (max-width: 600px){
+        line-height: 15px;
+        padding-top:0.5em;
+        padding-left:2em;
+    }
+`
+const Topic = styled(Small2)`
+    font-family: Quark;
+    font-size: 16px;
+    line-height: 22px;
+    text-align: center;
+    letter-spacing: 0.05em;
+    color: #FFFFFF;
+    position:absolute;
+    z-index: 4;
+    padding-top:0.7em;
+    padding-left:3.4em;
+    @media screen and (max-width: 600px) {
+        margin-left:16.2em;
+        margin-top:-0.1em;
+    }
+`
+
 export default class DS extends Component {
     render() {
         return (
             <DSbg>
                 <Bird src="/images/Objects/c2.png" />
+                <Topic>
+                    Design
+                </Topic>
+                <Text>
+                    <div>Lorem Ipsum is simply </div>
+                    <div>dummy text of the printing  </div>
+                    <div>type setting industry. </div>
+                    <div>Lorem Ipsum has been the  </div>
+                    <div>industry's standard dummy </div>
+                    <div>text ever</div>   
+                </Text>
                 <Frame src="/images/Objects/frame.png" />
             </DSbg>
         )
