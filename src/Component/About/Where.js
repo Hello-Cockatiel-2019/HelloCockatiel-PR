91e8652a03521f395e66794dca4582a25a59dcab
Poony <poon.002@hotmail.com>
2019-09-08 23:24:34 +0700

    [UPADTE]Responsive

--- src/Component/About/Where.js
+++ src/Component/About/Where.js
@@ -1,61 +1,80 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
-import { Paragraph } from '../Core/Text'
+import { Small } from '../Core/Text'
 
 const Where1 = styled.div `
 @media(min-width:320px) {
     
-    width: 85px;
+    width:85px;
     height: 25px;
-}
-@media (min-width:410px) {
-    height: 3.4375em;
-    width: 11.3125em;
-}
-@media (min-width:576px) {
-    height: 3.4375em;
-    width: 11.3125em;
-}
+  }
+  @media (min-width:410px) {
+    
+    width:109px;
+    height: 32px;
+  }
+  @media (min-width:768px) {
+    
+    width:97px;
+    height: 30px;
+    
+  }
+  @media (min-width:960px) {
+    
+    width:121px;
+    height: 37px;
+    
+  }
+  @media(min-width:1024px) {
+    
+    width:129px;
+    height: 39px;
+  }
+  
     background-image: url(/images/Objects/arrowplate3.png);
     background-repeat: no-repeat;
     background-size:100%;
-    padding-left:2em;
-    margin-left:0.1em;
+    // padding-left:2em;
+    // margin-left:0.1em;
+    // box-shadow:0 5px 5px black;
     
 `
 const Sit = styled.div `
-    background-image: url(/images/Objects/arrowplate2.png);
     @media(min-width:320px) {
+        width:131px;
         height: 38px;
-        width: 131px;
-    }
-    @media (min-width:410px) {
-        height: 4.9375em;
-        width: 17.1875em;
-    }
-    @media (min-width:576px) {
-        height: 4.9375em;
-        width: 17.1875em;
-    }
-    background-repeat: no-repeat;
-    background-size:100%;
-    padding-left:0.5em;
-    margin-top:1em;
+      }
+      @media (min-width:410px) {
+        width:168px;
+        height: 49px;
+      }
+      @media (min-width:768px) {
+        width:132px;
+        height: 73px;
+        
+      }
+      @media (min-width:960px) {
+        width:165px;
+        height: 48px;
+      }
+      @media(min-width:1024px) {
+        width:176px;
+        height: 51px;
+      }
+        background-image: url(/images/Objects/arrowplate2.png);
+        background-repeat: no-repeat;
+        background-size:100%;
+        // padding-left:2em;
+        // margin-left:0.1em;
+        // box-shadow:0 5px 5px black;
+    `
+
+const Text= styled(Small) `
+    // transform: translateY(50%);
+    
 `
 
-const Text= styled(Paragraph) `
-    transform: translateY(50%);
-    @media(min-width:320px) {
-        
-    }
-    @media (min-width:410px) {
-        
-    }
-    @media (min-width:576px) {
-        
-    }
-`
 
 export default class Where extends Component{
     render() {
@@ -69,7 +88,7 @@ export default class Where extends Component{
                 </Where1>
                 <Sit>
                     <Text>
-                    คณะเทคโนโลยี
+                    คณะเทคโนโลยี<br/>
                     สารสนเทศ
                     </Text>
                 </Sit>
--- src/Component/About/index.js
+++ src/Component/About/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import Where from './Where'
 
-import { Paragraph } from '../Core/Text'
+import { Paragraph, Subtitle } from '../Core/Text'
 
 const Plate = styled.div `
     
@@ -13,67 +13,104 @@ const Plate = styled.div `
       }
       @media (min-width:410px) {
         background-image: url(/images/Objects/small-plate.png);
-        width:340px;
-        height: 283px;
+        width:300px;
+        height: 250px;
       }
-    //   @media (min-width:576px) {
-    //     background-image: url(/images/Objects/small-plate.png);
-    //     width:470px;
-    //     height: 443px;
-    //   }
-    //   @media (min-width:768px) {
-    //     background-image: url(/images/Objects/big-plate.png);
-    //     width:740px;
-    //     height: auto;
-    //   }
-    //   @media(min-width:1024px) {
-    //     background-image: url(/images/Objects/big-plate.png);
-    //     height: 25.1875em;
-    //     width: auto;
-    //   }
-    //   @media only screen and (min-width: 768px) and (max-height: 1024px) and (orientation: portrait)  {
-    //     background-image: url(/images/Objects/big-plate.png);
-    //     height: 25.1875em;
-    //     width: auto;
-    //   }
-    //   @media only screen and (min-width: 1024px) and (max-height: 1370px) and (orientation: portrait)  {
-    //     background-image: url(/images/Objects/big-plate.png);
-    //     height: 25.1875em;
-    //     width: 43.75em;
-    //   } 
+      @media only screen and (min-width: 768px) and (max-height: 1024px) and (orientation: portrait)  {
+        background-image: url(/images/Objects/small-plate.png);
+        width:562px;
+        height: 469px;
+      }
+      @media only screen and (min-width: 768px) and (max-height: 1024px) and (orientation:landscape)  {
+        background-image: url(/images/Objects/big-platebird.png);
+        width:383px;
+        height: 240px;
+      }
+    
+      
+      @media (min-width:960px) {
+        background-image: url(/images/Objects/big-platebird.png);
+        width:478px;
+        height: 300px;
+        
+      }
+      @media(min-width:1024px) {
+        background-image: url(/images/Objects/big-platebird.png);
+        width:510px;
+        height: 320px;
+      }
       background-size:100%; 
-    background-repeat: no-repeat;
-    // size:100%;
-    
+      background-repeat: no-repeat;
+      size:100%;
+      background-size:100%; 
+      background-repeat: no-repeat;
+      size:100%;
+      
 `
-const Text= styled(Paragraph) `
-    @media(min-width:320px) {
-    
-    }
-    @media (min-width:410px) {
-    
-    }
-    @media (min-width:576px) {
-    
-    }
-`
+      const Title= styled(Subtitle) `
+      @media(min-width:320px) {
+        margin-top:5px;
+      }
+      @media (min-width:410px) {
+        margin-top:7px;
+      }
+      @media only screen and (min-width: 768px) and (max-height: 1024px) and (orientation: portrait)  {
+        margin-top:10px;
+        font-size:28px;
+      }
+      @media only screen and (min-width: 768px) and (max-height: 1024px) and (orientation:landscape)  {
+        margin-top:27px;
+      }
+      @media (min-width:960px) {
+        margin-left:1px;
+        margin-top:29px;
+      }
+      @media(min-width:1024px) {
+        margin-left:3px;
+        margin-top:32px;
+      }
+      text-shadow:0 5px 5px black;
+      `
+      const Content= styled(Paragraph) `
+      @media(min-width:320px) {
+        margin-left:30px;
+        margin-top:5px;
+      }
+      @media (min-width:410px) {
+        margin-left:35px;
+        margin-top:10px;
+      }
+      @media (min-width:768px) {
+        margin-left:42px;
+        margin-top:18px;
+      }
+      @media (min-width:960px) {
+        margin-left:44px;
+        margin-top:20px;
+      }
+      @media(min-width:1024px) {
+        margin-left:50px;
+        margin-top:24px;
+      }
+      
+      `
+
 
 const AboutBox = () => (
     
         <div class="d-flex flex-column">
             
             <Plate>
-                <Text className="mt-3 text-center">
+                <Title className="text-center">
                     About us
-                </Text>
+                </Title>
             
-            <Text className="ml-5 mt-5">
+            <Content>
                 text
-            </Text>
+            </Content>
             </Plate>
             <Where />
         </div>
-    
   )
 
 export default class About extends Component {
