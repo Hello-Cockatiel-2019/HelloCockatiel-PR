6daf71b66a852a091d55779b6cdfdbf4445fe057
Unknows <firstpan555@gmail.com>
2019-09-09 18:21:14 +0700

    [UPDATE]Shadow Register Button

--- src/Component/Core/Register.js
+++ src/Component/Core/Register.js
@@ -9,6 +9,25 @@ const Button = styled.button`
     background-size:100%;
     height:90px;
     width:200px;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25);
+     @media(max-width:576px) {
+      box-shadow: 0px 0px 4px #000000, 0px 0px 4px #000000;
+    }
+    @media only screen and (min-width: 768px) and (max-height: 1024px) and (orientation: portrait)  {
+      box-shadow: 0px 0px 4px #000000, 0px 0px 4px #000000;
+    }
+    @media only screen and (min-width: 834px) and (max-height: 1112px) and (orientation: portrait)  {
+      box-shadow: 0px 0px 4px #000000, 0px 0px 4px #000000;
+    }
+    @media only screen and (min-width: 834px) and (max-height: 1196px) and (orientation: portrait) {
+       box-shadow: 0px 0px 4px #000000, 0px 0px 4px #000000;
+    }
+    @media only screen and (min-width: 1024px) and (max-height: 1370px) and (orientation: portrait)  {
+      box-shadow: 0px 0px 4px #000000, 0px 0px 4px #000000;
+    }
+    @media only screen and (min-width: 1024px) and (max-height: 1468px) and (orientation: portrait) {
+      box-shadow: 0px 0px 4px #000000, 0px 0px 4px #000000;
+    } 
 `
 
 const A = styled.a`
