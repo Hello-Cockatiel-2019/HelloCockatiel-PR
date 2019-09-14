aae86079c7cb643c88290cc3f108bb7f8231147a
poomktw <kittiwat.ktw@mail.kmutt.ac.th>
2019-09-14 22:01:46 +0700

    [UPDATE]responsive

--- src/Component/Major/Spacing.js
+++ src/Component/Major/Spacing.js
@@ -1,7 +1,16 @@
 import styled from 'styled-components'
 
 const Spacing = styled.div`
-    padding:0.5em;
+    padding:1em;
+    @media (min-width: 320px){
+        padding:0.5em;
+    }
+    @media (min-width: 414px){
+        padding:1em;
+    }
+    @media (min-width: 768px){
+        padding:1.5em;
+    }
 `
 
 export default Spacing ;
