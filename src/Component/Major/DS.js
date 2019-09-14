17ce58fda000746508ff9aaba55ad6cc0986f986
poomktw <kittiwat.ktw@mail.kmutt.ac.th>
2019-09-14 22:02:58 +0700

    [UPDATE]add responsive (all f12)

--- src/Component/Major/DS.js
+++ src/Component/Major/DS.js
@@ -9,20 +9,63 @@ const DSbg = styled.div`
     border-radius: 30px;
     width: 27em;
     height: 10em;
-    @media screen and (max-width: 600px){
-        width:19em;
-        height:7em;
+    @media (max-width: 320px){
+        width:18em;
+        height:7.5em;
+    }
+    @media (min-width: 321px) and (max-width:375px){
+        width:18em;
+        height:7.5em;
+        margin-left:1.6em;
+    }
+    @media (min-width: 376px) and (max-width:414px){
+        width:18em;
+        height:7.5em;
+        margin-left:3em;
+    }
+    @media only screen and (min-width: 768px) and (max-height: 1024px) and (orientation: portrait){
+        width:28em;
+        height:10em;
+        margin-left:7em;
+    }
+    @media only screen and (min-width: 1024px) and (max-height: 768px) and (orientation: landscape){
+        width:25em;
+        height:10em;
+    }
+    @media only screen and (min-width: 1024px) and (max-height: 1366px) and (orientation: portrait){
+        width:32em;
+        height:10em;
+        margin-left:13em;
+    }
+    @media only screen and (min-width: 1366px) and (max-height: 1024px) and (orientation: landscape){
+        width:28em;
+        height:12em;
     }
 `
 const Bird = styled.img`
     padding-left:2em;
     padding-top:0.7em;
     width:7.5em;
-    @media screen and (max-width: 600px){
+    @media (max-width: 320px){
         width:6em;
         margin-top:-0.3em;
         transform: matrix(-1, 0, 0, 1, 0, 0);
-        margin-left:13.5em;
+        margin-left:13em;
+    }
+    @media (min-width: 321px) and (max-width:375px){
+        width:6em;
+        margin-top:-0.3em;
+        transform: matrix(-1, 0, 0, 1, 0, 0);
+        margin-left:13em;
+    }
+    @media (min-width: 376px) and (max-width:414px){
+        width:6em;
+        margin-top:-0.3em;
+        transform: matrix(-1, 0, 0, 1, 0, 0);
+        margin-left:13em;
+    }
+    @media only screen and (min-width: 1024px) and (max-height: 1366px) and (orientation: portrait) {
+        margin-left:1.5em;
     }
 `
 const Frame = styled.img`
@@ -32,10 +75,23 @@ const Frame = styled.img`
     z-index: 2;
     padding-top:6.2em;
     margin-left:-5.7em;
-    @media screen and (max-width: 600px){
+    @media (max-width: 320px){
         width:4.5em;
-        margin-left:13.5em;
+        margin-left:12.6em;
         padding-top:0em;
+    }
+    @media (min-width: 321px) and (max-width:375px){
+        width:4.5em;
+        margin-left:12.6em;
+        padding-top:0em;
+    }
+    @media (min-width: 376px) and (max-width:414px){
+        width:4.5em;
+        margin-left:12.6em;
+        padding-top:0em;
+    }
+    @media only screen and (min-width: 768px) and (max-height: 1024px) and (orientation: portrait){
+        
     }
 `
 const Text = styled(Small)`
@@ -48,10 +104,48 @@ const Text = styled(Small)`
     padding-left:8em;
     margin-top:-4.8em;
     color: #000000;
-    @media screen and (max-width: 600px){
+    padding-right:8em;
+    @media (max-width: 320px){
         line-height: 15px;
         padding-top:0.5em;
         padding-left:2em;
+        padding-right:8em;
+    }
+    @media (min-width: 321px) and (max-width:375px){
+        line-height: 15px;
+        padding-top:0.5em;
+        padding-left:2em;
+        padding-right:9em;
+    }
+    @media (min-width: 376px) and (max-width:414px){
+        line-height: 15px;
+        padding-top:0.5em;
+        padding-left:2em;
+        padding-right:11em;
+    }
+    @media only screen and (min-width: 768px) and (max-height: 1024px) and (orientation: portrait){
+        line-height: 25px;
+        margin-top:-6em;
+        margin-left:4em;
+        padding-right:13em;
+    }
+    @media only screen and (min-width: 1024px) and (max-height: 768px) and (orientation: landscape){
+        line-height: 25px;
+        margin-top:-5em;
+        margin-left:0em;
+        padding-right:4em;
+    }
+    @media only screen and (min-width: 1024px) and (max-height: 1366px) and (orientation: portrait){
+        line-height: 25px;
+        margin-top:-6em;
+        margin-left:7em;
+        padding-right:20em;
+    }
+    @media only screen and (min-width: 1366px) and (max-height: 1024px) and (orientation: landscape){
+        line-height: 25px;
+        margin-top:-5em;
+        margin-left:1em;
+        padding-right:9em;
     }
 `
 const Topic = styled(Small2)`
@@ -65,9 +159,24 @@ const Topic = styled(Small2)`
     z-index: 4;
     padding-top:0.7em;
     padding-left:3.4em;
-    @media screen and (max-width: 600px) {
-        margin-left:16.2em;
+    @media (max-width: 320px) {
+        margin-left:15em;
         margin-top:-0.1em;
+    }
+    @media (min-width: 321px) and (max-width:375px){
+        margin-left:15em;
+        margin-top:-0.1em;
+    }
+    @media (min-width: 376px) and (max-width:414px){
+        margin-left:15em;
+        margin-top:-0.1em;
+    }
+    @media only screen and (min-width: 768px) and (max-height: 1024px) and (orientation: portrait){
+        margin-left:1.5em;
+        margin-top:0.3em;
+    }
+    @media only screen and (min-width: 1024px) and (max-height: 1366px) and (orientation: portrait){
+        margin-left:3.5em;
     }
 `
 
@@ -80,12 +189,9 @@ export default class DS extends Component {
                     Design
                 </Topic>
                 <Text>
-                    <div>Lorem Ipsum is simply </div>
-                    <div>dummy text of the printing  </div>
-                    <div>type setting industry. </div>
-                    <div>Lorem Ipsum has been the  </div>
-                    <div>industry's standard dummy </div>
-                    <div>text ever</div>   
+                    <div>
+                    ในสาขานี้จะไปลงลึกเกี่ยวกับ วิธีการและ ขั้นตอนในการ Design หน้าเว็บไซต์ให้ เหมาะสมและน่าใช้งาน โดยจะเริ่มปูพื้นฐาน ตั้งแต่ องค์ประกอบของศิลป์ ลิขสิทธิ์ การ Drawing UX/UI ไปจนถึงการทำ Prototype ของงาน
+                    </div>
                 </Text>
                 <Frame src="/images/Objects/frame.png" />
             </DSbg>
