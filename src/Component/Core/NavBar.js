70f92b2c73398414e3e08b6130f1045fdf83b08f
Unknows <firstpan555@gmail.com>
2019-09-15 21:29:59 +0700

    [UPDATE]ADD Logo in Desktop Main page

--- src/Component/Core/NavBar.js
+++ src/Component/Core/NavBar.js
@@ -9,13 +9,22 @@ import NavButton from './NavButton';
 import Register from './Register';
 import ClickChage from './ClickChange';
 
+const Div = styled.div`
+    transform :  translateY(-50%);
+`
+
 const SideBarSection = styled.div`
     transform :  translateY(65%);
-    
 `
 
 const RegiserButton = styled.div`
     transform:translate(-20%,20%);
+`
+
+const Logo = styled.img`
+    width: 693.28px;
+    height: 262px;
+    top: 19px;
 `
 
 export default class NavBar extends Component {
@@ -34,7 +43,10 @@ export default class NavBar extends Component {
         return (
             <React.Fragment>
                 <Desktop className="container-fluid">
-                    <div className="row justify-content-between">
+                    <div className="d-flex justify-content-center">
+                        <Logo src="/images/Objects/Logo.png" />
+                    </div>
+                    <Div className="row justify-content-between">
                         <SideBarSection className="col-2 d-inline-flex flex-column ml-5">
                             <NavButton changeClick={this.changeClick} />
                         </SideBarSection>
@@ -42,7 +54,7 @@ export default class NavBar extends Component {
                         <RegiserButton className="col-3 col-xl-2 d-inline-flex">
                             <Register />
                         </RegiserButton>
-                    </div>
+                    </Div>
                 </Desktop>
                 <Mobile>
                     <ClickChage keyid={this.state.id} clickMobile={this.changeClick} />
