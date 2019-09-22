fedf6838a108ef1efec0eaad9c0634b3759becf3
Unknows <firstpan555@gmail.com>
2019-09-22 16:11:11 +0700

    [UPDATE]Fixed bug Background

--- src/Component/Core/Background.js
+++ src/Component/Core/Background.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 const FixedBackground = styled.div`
   z-index:-2;
   width:100%;
-  height: 100%;
+  height: 100vh;
   background-repeat:no-repeat;
   background-position: bottom;
   position:fixed;
@@ -12,19 +12,15 @@ const FixedBackground = styled.div`
   background-image:url('/images/Background/1536x792.png');
   
     @media (max-width: 960px) {
-      height: 100vh;
       background-image:url('/images/Background/768x1230.png');
     }
     @media only screen and (max-width: 375px) and (min-height: 568px) and (orientation:portrait) {
-      height: 100vh;
       background-image:url('/images/Background/414x896.png');
     }
     @media only screen and (min-width: 375px) and (max-height: 736px) and (orientation:portrait) {
-      height: 100vh;
       background-image:url('/images/Background/375x632.png');
     }
     @media (max-width: 320px) {
-      height: 100vh;
       background-image:url('/images/Background/375x632.png');
     }
 `
