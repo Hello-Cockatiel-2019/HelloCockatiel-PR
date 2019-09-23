63f0a59ca2f9f55db148c9fc59f14060f6eee81c
Unknows <firstpan555@gmail.com>
2019-09-23 20:15:01 +0700

    [UPDATE]Reimage bckground

--- src/Component/Core/Background.js
+++ src/Component/Core/Background.js
@@ -11,25 +11,14 @@ const FixedBackground = styled.div`
   background-size:cover;
   background-image:url('/images/Background/bg1536x900.png');
 
-    @media (max-width: 1536px) and (max-height:793px) {
-      background-image:url('/images/Background/bg1536x792.png');
-    }
     @media (max-width: 1440px){
-      background-position: center;
       background-image:url('/images/Background/bg1440x1024.png');
-    }
-    @media (max-width: 1196px){
-      background-position: bottom;
-      background-image:url('/images/Background/bg1196x834.png');
     }
     @media (max-width: 768px) {
       background-image:url('/images/Background/bg768x1000.png');
     }
     @media only screen and (max-width: 414px){
       background-image:url('/images/Background/bg414x896.png');
-    }
-    @media only screen and (max-width: 375px){
-      background-image:url('/images/Background/bg375x632.png');
     }
 `
 export default class background extends Component {
