653aded5d54a4cae3fdab0a256ca4d1dc24f47da
Unknows <firstpan555@gmail.com>
2019-09-23 16:34:00 +0700

    [UPDATE]New Background Size

--- src/Component/Core/Background.js
+++ src/Component/Core/Background.js
@@ -15,13 +15,15 @@ const FixedBackground = styled.div`
       background-image:url('/images/Background/bg1536x792.png');
     }
     @media (max-width: 1440px){
+      background-position: center;
       background-image:url('/images/Background/bg1440x1024.png');
     }
     @media (max-width: 1196px){
+      background-position: bottom;
       background-image:url('/images/Background/bg1196x834.png');
     }
     @media (max-width: 768px) {
-      background-image:url('/images/Background/bg768x1230.png');
+      background-image:url('/images/Background/bg768x1000.png');
     }
     @media only screen and (max-width: 414px){
       background-image:url('/images/Background/bg414x896.png');
@@ -29,9 +31,6 @@ const FixedBackground = styled.div`
     @media only screen and (max-width: 375px){
       background-image:url('/images/Background/bg375x632.png');
     }
-    /* @media (max-width: 320px) {
-      background-image:url('/images/Background/bg1536x792.png');
-    } */
 `
 export default class background extends Component {
   render () {
